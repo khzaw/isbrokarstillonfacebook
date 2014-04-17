9ac6333280fc177ca44cac1ce4983c25152801d8
Kaung Htet Zaw <emoosx@gmail.com>
2014-04-18 05:30:42 +0800

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$(function() {
+  $('.question').textillate({ 
+    in: { effect: 'fadeIn', shuffle: true, delayScale: 1.5 },
+  });
+
+  var brokar = '1431546801';
+  $.get('http://graph.facebook.com/' + brokar, function(data, status, xhr) {
+    $(".answer").text("Yes!");
+  }).fail(function() {
+    $(".answer").text("No :(");
+  }).always(function() {
+    $(".answer").show({duration: 1200});
+  });
+});
---
+++ main.py
@@ -0,0 +1,17 @@
+import webapp2
+import jinja2
+import os
+
+jinja_environment = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__))
+)
+
+
+class HomePage(webapp2.RequestHandler):
+    def get(self):
+        template_values = {}
+        template = jinja_environment.get_template('index.html')
+        self.response.out.write(template.render(template_values))
+
+
+app = webapp2.WSGIApplication([('/', HomePage)], debug=True)
